18518ead5380fdd7b40c5fb640c794de32cf7566
Cristina <cristina.manzoni@outlook.com>
2020-05-23 00:34:40 +0200

    first commit setting up the basics

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <nav>
+      <li><a href='#app'>Cristina Manzoni</a></li>
+    </nav>
+    <img alt="wave" src="./assets/wave.svg">
+    <Hello/>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  height: 60px;
+}
+</style>
